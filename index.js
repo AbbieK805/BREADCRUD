90f87fc657066b9f3e71e0da3dab79b61b12669d
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-24 19:24:57 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT
+const breadRoutes = require('./controllers/breads_controller')
+const methodOverrid = require('method-override')
+
+const app = express()
+
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverrid('_method'))
+
+
+app.use('/breads', breadRoutes)
+
+app.get('/', (req, res) => {
+    res.send("Welcome to BREAD!")
+})
+
+app.listen(PORT, () => console.log(`Live and worldwide at port ${PORT}`))
