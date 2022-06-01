7b3821257d23a9331b6d54b3432945efb0000e78
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-31 20:04:48 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+require('dotenv').config()
+const PORT = process.env.PORT
+const breadRoutes = require('./controllers/breads_controller')
+const methodOverrid = require('method-override')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverrid('_method'))
+
+
+app.use('/breads', breadRoutes)
+
+app.get('/', (req, res) => {
+    res.send("Welcome to BREAD!")
+})
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('DB is locked and loaded'))
+    .catch(err => console.error(err));
+
+app.listen(PORT, () => console.log(`Live and worldwide at port ${PORT}`))
