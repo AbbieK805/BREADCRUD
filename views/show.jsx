ba155555179a8ecf48a70ba8b72408cf81161c31
AbbieK805 <97373292+AbbieK805@users.noreply.github.com>
2022-05-31 20:01:55 -0500

    Update show.jsx

--- views/show.jsx
+++ views/show.jsx
@@ -1,18 +1,18 @@
-
 const React = require('react')
 const Default = require('./layouts/default')
 
-function Show({ bread, index }) {
+function Show({ bread }) {
     return (
         <Default>
             <h2>Show page</h2>
             <h3>{bread.name}</h3>
             <p>
-                and it {bread.hasGluten ? <span> does</span> : <span> does NOT</span>} have gluten.
+                and it { bread.hasGluten ? <span> does</span> : <span> does NOT</span>  } have gluten.
             </p>
-            <img src={bread.image} alt={bread.name} />
-            <a href={`/breads/${index}/edit`} >Edit</a>
-            <form method='POST' action={`/breads/${index}?_method=DELETE`}>
+            <img src={bread.image} alt={bread.name}/>
+            <p>{bread.getBakedBy()}</p>
+            <a href={`/breads/${bread._id}/edit`} >Edit</a>
+            <form method='POST' action={`/breads/${bread._id}?_method=DELETE`}>
                 <input type="submit" value="DELETE" />
             </form>
         </Default>
